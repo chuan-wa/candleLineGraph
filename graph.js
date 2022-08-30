7f7226e30e0337d5f84a27ea051396fa6dd2af89
chuan-wa <1379499077@qq.com>
2022-08-30 08:50:48 +0800

    可拖动

--- graph.js
+++ graph.js
@@ -29,13 +29,18 @@ var CandleGraph = {
         canvasID: "candleCanvas",
         isMinute: true,
         time: "updateTime",
-        date: "date",
+        date: "displayTime",
         riseColor: "red",
-        fallColor: "green"
+        fallColor: "green",
+        dataNumShown: 30,
+        dataEnd: 0
     },
     wordYAixsChange: 4,
     c: null,
     ctx: null,
+    isClick: false,
+    delta:10,
+
 
     origin: [0, 0],
 
@@ -47,12 +52,12 @@ var CandleGraph = {
 
     draw: function () {
         //define inside variable
-        let { wordYAixsChange, c, ctx,origin } = this
-        let { subyAxisFactor, subDensity, volume, rectInterval, padding, xPixel, yPixel, mainxAxisFactor, mainyAxisFactor, mainDensity, canvasID, openPrice, closePrice, maxPrice, minPrice, PATH, date, elementList, ELPostion, isMinute, timeLineAndInterval, time, fallColor, riseColor } = this.options
+        let { wordYAixsChange, c, ctx, origin } = this
+        let { dataEnd, dataNumShown, subyAxisFactor, subDensity, volume, rectInterval, padding, xPixel, yPixel, mainxAxisFactor, mainyAxisFactor, mainDensity, canvasID, openPrice, closePrice, maxPrice, minPrice, PATH, date, elementList, ELPostion, isMinute, timeLineAndInterval, time, fallColor, riseColor } = this.options
         try {
             c = document.getElementById(canvasID)
             ctx = c.getContext("2d")
-            ctx.lineWidth = 0.5, ctx.font = "15 px sans-serif", ctx.strokeStyle = "grey", ctx.fillStyle = "black"
+            ctx.lineWidth = 0.5, ctx.font = "12px sans-serif", ctx.strokeStyle = "grey", ctx.fillStyle = "black"
         } catch {
             console.log("please write correct canvasID")
             return
@@ -80,8 +85,16 @@ var CandleGraph = {
             return
         }
 
-        var maxValue = Math.max(...minData[elementList].map(o => o[maxPrice]))
-        var minValue = Math.min(...minData[elementList].map(o => o[minPrice]))
+        if(dataEnd>minData[elementList].length - dataNumShown)this.options.dataEnd=minData[elementList].length - dataNumShown
+        var selectedData = minData[elementList].filter(function (el, index) {
+            return index >= minData[elementList].length - dataNumShown - dataEnd && index <= minData[elementList].length - dataEnd;
+        })
+
+        console.log(selectedData)
+
+
+        var maxValue = Math.max(...selectedData.map(o => o[maxPrice]))
+        var minValue = Math.min(...selectedData.map(o => o[minPrice]))
         var gap = maxValue - minValue
         maxValue += gap / (2 * mainDensity), minValue -= gap / (2 * mainDensity)
 
@@ -132,7 +145,7 @@ var CandleGraph = {
         //require to read data from strat to end
         //can be inserted in drawing KLine component
 
-        var dataSample = minData[elementList].length //fake data
+        var dataSample = selectedData.length //fake data
 
 
         var xAxisUnit = displayXAixs / dataSample  //erase two points at ends
@@ -147,23 +160,26 @@ var CandleGraph = {
 
         //-------------------------------------------
 
-        var temp = null   //for compare date
+        var temp = null, startTime = null, endTime = null   //for compare date
+
         count = 0
 
-        for (const value of minData[elementList]) {
+        for (const value of selectedData) {
             //minute graph
             if (isMinute) {
                 ctx.strokeStyle = "grey", ctx.fillStyle = "black"
-                if (count == 0) { ctx.fillText(value[time], -xAxisUnit / 2 - ctx.measureText(value[time]).width / 2, displayYAixs + padding[3]); drawLine([-xAxisUnit / 2, 0], [-xAxisUnit / 2, displayYAixs]); drawLine([-xAxisUnit / 2, displayYAixs + padding[2] + padding[3]], [-xAxisUnit / 2, displayYAixs + padding[2] + padding[3] + subdisplayYAixs]) }
-                if (count == dataSample - 1) { ctx.fillText(value[time], displayXAixs - xAxisUnit / 2 - ctx.measureText(value[time]).width / 2, displayYAixs + padding[3]); drawLine([displayXAixs - xAxisUnit / 2, 0], [displayXAixs - xAxisUnit / 2, displayYAixs]); drawLine([displayXAixs - xAxisUnit / 2, displayYAixs + padding[2] + padding[3]], [displayXAixs - xAxisUnit / 2, displayYAixs + padding[2] + padding[3] + subdisplayYAixs]) }
+                if (count == 0) { startTime = value[time], ctx.fillText(startTime, -xAxisUnit / 2 - ctx.measureText(value[time]).width / 2, displayYAixs + padding[3]); drawLine([-xAxisUnit / 2, 0], [-xAxisUnit / 2, displayYAixs]); drawLine([-xAxisUnit / 2, displayYAixs + padding[2] + padding[3]], [-xAxisUnit / 2, displayYAixs + padding[2] + padding[3] + subdisplayYAixs]) }
+
                 if (temp != null) {
                     if (calculateTimeStamp(temp[time], value[time]) != 1) {
                         ctx.fillText(cohenseTime(temp[time], value[time]), count * xAxisUnit - ctx.measureText(cohenseTime(temp[time], value[time])).width / 2, displayYAixs + padding[3])
                         drawLine([count * xAxisUnit, 0], [count * xAxisUnit, displayYAixs])
                         drawLine([count * xAxisUnit, displayYAixs + padding[2] + padding[3]], [count * xAxisUnit, displayYAixs + padding[2] + padding[3] + subdisplayYAixs])
                     }
+
                 }
                 //fill mouse pointer time info
+                if (count == dataSample - 1) { endTime = value[time], ctx.fillText(value[time], displayXAixs - xAxisUnit / 2 - ctx.measureText(value[time]).width / 2, displayYAixs + padding[3]); drawLine([displayXAixs - xAxisUnit / 2, 0], [displayXAixs - xAxisUnit / 2, displayYAixs]); drawLine([displayXAixs - xAxisUnit / 2, displayYAixs + padding[2] + padding[3]], [displayXAixs - xAxisUnit / 2, displayYAixs + padding[2] + padding[3] + subdisplayYAixs]) }
 
                 ctx.fillStyle = "black"
             }
@@ -205,7 +221,10 @@ var CandleGraph = {
         ctx.save()
         ctx.translate(0, displayYAixs + padding[3] + padding[2])
 
-        var maxVolume = Math.max(...minData[elementList].map(o => o[volume]))
+        var maxVolume = Math.max(...selectedData.map(o => o[volume]))
+        //leave blank space
+        maxVolume += maxVolume / 10
+        //
         yAixsValue = []
         ctx.strokeStyle = "grey"
         for (var i = 0; i <= subDensity; i++) {
@@ -215,7 +234,7 @@ var CandleGraph = {
         }
 
         var subyAxisUnit = subdisplayYAixs / maxVolume
-        for (const value of minData[elementList]) {
+        for (const value of selectedData) {
             if (value[openPrice] <= value[closePrice]) { ctx.fillStyle = riseColor, ctx.strokeStyle = riseColor }
             if (value[openPrice] > value[closePrice]) { ctx.fillStyle = fallColor, ctx.strokeStyle = fallColor }
             ctx.fillRect((count - (1 - rectInterval) / 2) * xAxisUnit, (maxVolume) * subyAxisUnit, xAxisUnit * (1 - rectInterval), -value[volume] * subyAxisUnit)
@@ -224,6 +243,9 @@ var CandleGraph = {
         this.origin = [-padding[0] - xAxisUnit / 2, -displayYAixs - 2 * padding[2] - padding[3]]
 
 
+
+
+        //#########
         function interact() {
             function isInMainAfter() {
                 return !(mouse.x < 0 || mouse.x >= displayXAixs || mouse.y < 0 || mouse.y > displayYAixs)
@@ -242,68 +264,100 @@ var CandleGraph = {
                 mouse.y -= c.getBoundingClientRect().top + padding[2]
                 if (mouse.x % xAxisUnit < xAxisUnit / 2) { mouse.x = Math.floor((mouse.x / xAxisUnit)) * xAxisUnit }
                 else { mouse.x = (Math.ceil(mouse.x / xAxisUnit)) * xAxisUnit }
-
             }
 
             function drawCrossLine() {
-                ctx.strokeStyle="grey"
-                if (!(isInMainAfter() || isInSubBefore())) return
+                ctx.strokeStyle = "grey"
                 ctx.setLineDash([1, 2])
                 drawLine([0, mouse.y], [displayXAixs, mouse.y])
                 drawLine([mouse.x, 0], [mouse.x, displayYAixs + padding[2] + padding[3] + subdisplayYAixs])
                 ctx.setLineDash([1, 0])
-
-
             }
-            function transformYAxisToYValue(y,originy, unit, max) {
-                return (-(y-originy) / unit + max).toFixed(2)
+            function transformYAxisToYValue(y, originy, unit, max) {
+                return (-(y - originy) / unit + max).toFixed(2)
             }
 
 
             function fillCrossInfo() {
-                if (isInMainAfter() || isInSubBefore()) {
-                    ctx.fillStyle="black"
-                    var numOfSample = Math.floor(mouse.x / xAxisUnit)
-                    //console.log(numOfSample)
-                    var value = minData[elementList][numOfSample]
-                    ctx.fillRect(mouse.x - ctx.measureText(value[time]).width / 2 - 4, displayYAixs, ctx.measureText(value[time]).width + 8, padding[3] + 2)
-                    ctx.fillStyle = "white"
-                    ctx.fillText(value[time], mouse.x - ctx.measureText(value[time]).width / 2, displayYAixs + padding[3])
 
-                }
+                ctx.fillStyle = "black"
+                var numOfSample = Math.floor(mouse.x / xAxisUnit)
+                //console.log(numOfSample)
+                var value = selectedData[numOfSample]
+                ctx.fillRect(mouse.x - ctx.measureText(value[time]).width / 2 - 4, displayYAixs, ctx.measureText(value[time]).width + 8, padding[3] + 2)
+                ctx.fillStyle = "white"
+                ctx.fillText(value[time], mouse.x - ctx.measureText(value[time]).width / 2, displayYAixs + padding[3])
+
+
                 if (isInMainAfter()) {
-                    ctx.fillStyle="black"
-                    let tempInfo = transformYAxisToYValue(mouse.y,0, yAxisUnit, maxValue)
+                    ctx.fillStyle = "black"
+                    let tempInfo = transformYAxisToYValue(mouse.y, 0, yAxisUnit, maxValue)
                     //width of M is similar to any number's height
-                    ctx.fillRect(-padding[0]-4, mouse.y - 1 / 2 * ctx.measureText("M").width - 8, ctx.measureText(tempInfo).width + 8, 1 / 2 * ctx.measureText("M").width + 12)
+                    ctx.fillRect(-padding[0] - 4, mouse.y - 1 / 2 * ctx.measureText("M").width - 8, ctx.measureText(tempInfo).width + 8, 1 / 2 * ctx.measureText("M").width + 12)
                     ctx.fillStyle = "white"
-                    ctx.fillText(tempInfo,-padding[0], mouse.y)
+                    ctx.fillText(tempInfo, -padding[0], mouse.y)
                 }
-                if(isInSubBefore()){
-                    ctx.fillStyle="black"
-                    let tempInfo = transformYAxisToYValue(mouse.y,displayYAixs+padding[2]+padding[3],subyAxisUnit,maxVolume)
-                    ctx.fillRect(-padding[0]-4, mouse.y - 1 / 2 * ctx.measureText("M").width - 8, ctx.measureText(tempInfo).width + 8, 1 / 2 * ctx.measureText("M").width + 12)
+                if (isInSubBefore()) {
+                    ctx.fillStyle = "black"
+                    let tempInfo = transformYAxisToYValue(mouse.y, displayYAixs + padding[2] + padding[3], subyAxisUnit, maxVolume)
+                    ctx.fillRect(-padding[0] - 4, mouse.y - 1 / 2 * ctx.measureText("M").width - 8, ctx.measureText(tempInfo).width + 8, 1 / 2 * ctx.measureText("M").width + 12)
                     ctx.fillStyle = "white"
-                    ctx.fillText(tempInfo,-padding[0], mouse.y)
+                    ctx.fillText(tempInfo, -padding[0], mouse.y)
                 }
 
             }
+            //this horionzal center
+            //x is container length
+            function showWordsC(value, x, y) {
+                ctx.fillText(value, (x - ctx.measureText(value).width) / 2, y)
+            }
 
-            function detailInfo(){
-                let x =null
-                if(mouse.x>displayXAixs/2)x=-xAxisUnit/2
-                else x=displayXAixs*4/5-xAxisUnit/2
-                ctx.fillStyle="white"
-                ctx.fillRect(x,0,displayXAixs/5,displayYAixs/2)
-                ctx.strokeStyle="grey"
-                ctx.strokeRect(x,0,displayXAixs/5,displayYAixs/2)
-                ctx.translate(x,0)
+            function RorG(start, value) {
+                if (start < value) ctx.fillStyle = "red"
+                else if (start > value) ctx.fillStyle = "green"
+                else ctx.fillStyle = "black"
+            }
+
+            function detailInfo() {
+                let x = null
+                if (mouse.x > displayXAixs / 2) x = -xAxisUnit / 2
+                else x = displayXAixs * 2 / 3 - xAxisUnit / 2
+                let y = displayYAixs * 1 / 4, dx = displayXAixs / 3, dy = displayYAixs * 3 / 4
+                ctx.fillStyle = "white"
+                ctx.fillRect(x, y, dx, dy)
+                ctx.strokeStyle = "grey"
+                ctx.strokeRect(x, y, dx, dy)
+                ctx.translate(x, y)
                 var numOfSample = Math.floor(mouse.x / xAxisUnit)
-                var value = minData[elementList][numOfSample]
-                ctx.fillText(value[time],0,0)
+                var value = selectedData[numOfSample]
+                ctx.fillStyle = "black"
+                ctx.font = "15px sans-serif"
+                showWordsC(value[date] + " " + value[time], dx, dy / 10)
+                //console.log((2*x+dx)/2-ctx.measureText(value[date]+" "+value[time]).width/2)
+                showWordsC(" 开 盘 : " + value[openPrice], dx, 3 * dy / 10)
+                RorG(value[openPrice], value[closePrice])
+                showWordsC(" 收 盘 : " + value[closePrice], dx, 4 * dy / 10)
+                RorG(value[openPrice], value[maxPrice])
+                showWordsC(" 最 高 : " + value[maxPrice], dx, 5 * dy / 10)
+                RorG(value[openPrice], value[minPrice])
+                showWordsC(" 最 低 : " + value[minPrice], dx, 6 * dy / 10)
+                let symbol = null
+                if (value[openPrice] == value[closePrice]) symbol = ""
+                else if (value[openPrice] < value[closePrice]) symbol = "+"
+                else symbol = "-"
+                RorG(value[openPrice], value[closePrice])
+                showWordsC(" 收 跌 : " + symbol + Math.abs(value[openPrice] - value[closePrice]), dx, 7 * dy / 10)
+                RorG(value[openPrice], value[closePrice])
+                showWordsC(" 涨 跌 幅 : " + symbol + Math.abs((value[openPrice] - value[closePrice]) * 100 / value[openPrice]).toFixed(2) + "%", dx, 8 * dy / 10)
+                ctx.fillStyle = "blue"
+                showWordsC(" 成 交 量 : " + value[volume], dx, 9 * dy / 10)
+                ctx.fillStyle = "black"
+                ctx.translate(-x, -y)
+
             }
             translateMouseAxis()
-            ctx.translate(origin[0],origin[1])
+            if (mouse.x < 0 || mouse.x >= displayXAixs || mouse.y <= 0 || (mouse.y >= displayYAixs && mouse.y <= displayYAixs + padding[2] + padding[3]) || mouse.y > displayYAixs + padding[2] + padding[3] + subdisplayYAixs) return
+            ctx.translate(origin[0], origin[1])
             ctx.translate(padding[0] + xAxisUnit / 2, padding[2])
             drawCrossLine()
             fillCrossInfo()
@@ -311,8 +365,8 @@ var CandleGraph = {
             ctx.translate(0, displayYAixs + padding[3] + padding[2])
 
         }
-        if(typeof mouse.x !=="undefined")interact()
-    }
+        if (typeof mouse.x !== "undefined") interact()
+    },
 }
 
 
@@ -322,12 +376,39 @@ window.onload = function () {
     CandleGraph.draw()
 }
 
+window.addEventListener("mousedown", function (e) {
+    mouse.x = e.x
+    mouse.y = e.y
+})
+
+window.addEventListener("mouseup", function (e) {
+    var diffx = e.x - mouse.x
+    var diffy = e.y - mouse.y
+    if (Math.abs(diffx) < CandleGraph.delta) {
+        CandleGraph.isClick = !CandleGraph.isClick
+        if (CandleGraph.isClick == false) {
+            mouse.x = undefined, mouse.y = undefined
+        }
+    }
+    else {
+        CandleGraph.options.dataEnd = Math.max(0, CandleGraph.options.dataEnd+diffx / 100)
+        mouse.x = undefined, mouse.y = undefined
+        console.log(CandleGraph.options.dataEnd)
+    }
+    console.log(mouse)
+    CandleGraph.draw()
+
+}
+
+)
+
+
 
 
 window.addEventListener("mousemove", function (e) {
+    if (CandleGraph.isClick == false) return
     mouse.x = e.x;
     mouse.y = e.y;
-    if(isInMainAfter)
     CandleGraph.draw()
 });
 
